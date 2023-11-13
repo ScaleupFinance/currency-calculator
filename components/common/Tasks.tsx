3b6b80329951e92d7cb2bf8a44a1c6ae8e5f00ae
Kalle Pyörälä <kalle.pyorala@azurinspire.com>
2023-11-13 09:42:13 +0100

    LInks added to tasks and some help to swap button

--- components/common/Tasks.tsx
+++ components/common/Tasks.tsx
@@ -1,15 +1,21 @@
 "use client";
 
-import { useEffect, useState } from "react";
+import { Fragment, useEffect, useState } from "react";
 import { useLocalStorageValue } from "@react-hookz/web";
 
 const tasks = (path: string) => [
   {
     title: "CSS Styling",
-    link: {
-      label: "Figma File",
-      url: "https://www.figma.com/file/UP7twWG1B1iZgwyx2fstFl/Currency-Converter?type=design&mode=dev",
-    },
+    links: [
+      {
+        label: "Figma File",
+        url: "https://www.figma.com/file/UP7twWG1B1iZgwyx2fstFl/Currency-Converter?type=design&mode=dev",
+      },
+      {
+        label: "MDN CSS",
+        url: "https://developer.mozilla.org/en-US/docs/Web/CSS",
+      },
+    ],
     items: [
       `Center the content on the page, <a href='vscode://file/${path}/app/page.module.css'>app/page.module.css</a>`,
       `Fix the <pre>Form</pre> component styling, <a href='vscode://file/${path}/components/Form.module.css'>components/Form.module.css</a>`,
@@ -18,34 +24,48 @@ const tasks = (path: string) => [
   },
   {
     title: "Typescript",
-    link: null,
+    links: [
+      {
+        label: "Typescript",
+        url: "https://www.typescriptlang.org/docs",
+      },
+    ],
     items: [
       "Fix all Typescript errors in the project.<br/><pre>npm run build</pre> or <pre>yarn build</pre> must pass without errors",
     ],
   },
   {
     title: "API",
-    link: {
-      label: "Fetch API",
-      url: "https://developer.mozilla.org/en-US/docs/Web/API/Fetch_API/Using_Fetch",
-    },
+    links: [
+      {
+        label: "Fetch API",
+        url: "https://developer.mozilla.org/en-US/docs/Web/API/Fetch_API/Using_Fetch",
+      },
+    ],
     items: [
       `Replace hard coded currencies with data fetched from <pre>http://localhost:3000/api/currencies</pre> with Fetch API.<br/>Use either server side fetching on <a href='vscode://file/${path}/app/page.tsx'>app/page.tsx</a><br/>or client side fetching on <a href='vscode://file/${path}/components/Converter.tsx'>components/Converter.tsx</a><br/>To see the response, you can open <a href='/api/currencies' target='_blank'>API in browser</a>`,
     ],
   },
   {
     title: "Unit testing",
-    link: {
-      label: "Jest",
-      url: "https://jestjs.io/docs/getting-started",
-    },
+    links: [
+      {
+        label: "Jest",
+        url: "https://jestjs.io/docs/getting-started",
+      },
+      {
+        label: "React Testing Library",
+        url: "https://testing-library.com/docs/react-testing-library/intro",
+      },
+    ],
     items: [
-      `Write real unit tests to <a href='vscode://file/${path}/components/Form.test.tsx'>components/Form.test.tsx</a> file.<br/>Run tests with <pre>npm test</pre> or <pre>yarn test</pre> command`,
+      `Unit test in <a href='vscode://file/${path}/components/Form.test.tsx'>components/Form.test.tsx</a> has three placeholders for the tests for the <pre>Form</pre> component. What are the three most important things to test in this component?`,
+      `Start the test runner with <pre>npm test</pre> or <pre>yarn test</pre> command and write the three tests you chose`,
     ],
   },
   {
     title: "Bonus",
-    link: null,
+    links: null,
     items: ["Implement Swap button"],
   },
 ];
@@ -82,11 +102,16 @@ export const Tasks = ({ path }: TasksProps) => {
         <>
           <button onClick={() => setVisible(false)}>X</button>
           <h1>{task.title}</h1>
-          {task.link && (
+          {task.links && (
             <p>
-              <a target="_blank" href={task.link.url}>
-                {task.link.label}
-              </a>
+              {task.links.map((link, index) => (
+                <Fragment key={link.url}>
+                  <a target="_blank" href={link.url}>
+                    {link.label}
+                  </a>
+                  {index < task.links.length - 1 && " | "}
+                </Fragment>
+              ))}
             </p>
           )}
           <ol>
